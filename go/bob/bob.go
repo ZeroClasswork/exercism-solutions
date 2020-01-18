60189aa6e2aff56784de036c8309ad8148b4429f
megan <megan.obryan52@gmail.com>
2020-01-17 23:00:57 -0800

    Working on solution to bob

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -5,11 +5,38 @@
 // https://golang.org/doc/effective_go.html#commentary
 package bob
 
+import (
+	"strings"
+	"unicode"
+)
+
 // Hey should have a comment documenting it.
 func Hey(remark string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	for _, letter := range remark {
+		if letter != ' ' && letter != '\t' && letter != '\n' {
+			var hasCap = false
+			for _, letter := range remark {
+				if letter >= 'A' && letter <= 'Z' {
+					hasCap = true
+				} else if unicode.IsLetter(letter) {
+					if remark[len(remark)-1] == '?' {
+						return "Sure."
+					}
+					return "Whatever."
+				}
+			}
+			if hasCap {
+				if strings.ContainsAny(remark, "?") {
+					return "Calm down, I know what I'm doing!"
+				}
+				return "Whoa, chill out!"
+			} else {
+				if remark[len(remark)-1] == '?' {
+					return "Sure."
+				}
+				return "Whatever."
+			}
+		}
+	}
+	return "Fine. Be that way!"
 }
